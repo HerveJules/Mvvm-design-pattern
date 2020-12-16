a7d6c0baeb2d945d013a761894506a2da9e998fe
HerveJules <hervejulesiradukunda@gmail.com>
2020-12-16 12:10:48 +0200

    Initial MvvM settup

---
+++ MVVMEmployee/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using MVVMEmployee.Models;
+namespace MVVMEmployee.ViewModels
+{
+    public class EmployeeViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+        EmployeeService ObjEmployeeService;
+        public EmployeeViewModel()
+        {
+            ObjEmployeeService = new EmployeeService();
+            loadData();
+            CurrentEmployee = new Employee();
+        }
+        #region Display Operations
+        private List<Employee> employeesList;
+        public List<Employee> EmployeesList
+        {
+            get { return employeesList; }
+            set { employeesList = value; OnPropertyChanged("EmployeesList"); }
+        }
+        public void loadData()
+        {
+            EmployeesList = ObjEmployeeService.GetAll();
+        }
+        #endregion
+        private Employee currentEmployee;
+        public Employee CurrentEmployee
+        {
+            get { return currentEmployee; }
+            set { currentEmployee = value; OnPropertyChanged("CurrentEmployee"); }
+        }
+    }
+}
