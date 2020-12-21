53b00afc8c6414cbc032ed1bb70f10d153cb1593
HerveJules <hervejulesiradukunda@gmail.com>
2020-12-21 15:37:07 +0200

    CRUD Operations

--- MVVMEmployee/Models/EmployeeService.cs
+++ MVVMEmployee/Models/EmployeeService.cs
@@ -4,67 +4,174 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.ObjectModel;
 namespace MVVMEmployee.Models
 {
     public class EmployeeService
     {
-        private static List<Employee> ObjEmployeesList;
+        SqlConnection ObjSqlConnection;
+        SqlCommand ObjSqlCommand;
         public EmployeeService()
         {
-            ObjEmployeesList = new List<Employee>()
-            {
-                new Employee{ Id=111, Name="Herve", Age=25 }
-            };
+            ObjSqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnEmployee"].ConnectionString);
+            ObjSqlCommand = new SqlCommand();
+            ObjSqlCommand.Connection = ObjSqlConnection;
+            ObjSqlCommand.CommandType = CommandType.StoredProcedure;
         }
 
         public List<Employee> GetAll()
         {
-            return ObjEmployeesList;
+            List<Employee> ObjEmployeeList = new List<Employee>();
+            try
+            {
+                ObjSqlCommand.Parameters.Clear();
+                ObjSqlCommand.CommandText = "udp_SelectAllEmployees";
+
+                ObjSqlConnection.Open();
+                var ObjSqlDataReader = ObjSqlCommand.ExecuteReader();
+                if (ObjSqlDataReader.HasRows)
+                {
+                    Employee ObjEmployee = null;
+                    while (ObjSqlDataReader.Read())
+                    {
+                        ObjEmployee = new Employee();
+                        ObjEmployee.Id = ObjSqlDataReader.GetInt32(0);
+                        ObjEmployee.Name = ObjSqlDataReader.GetString(1);
+                        ObjEmployee.Age = ObjSqlDataReader.GetInt32(2);
+
+                        ObjEmployeeList.Add(ObjEmployee);
+                    }
+                }
+                ObjSqlDataReader.Close();
+            }
+            catch(SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                ObjSqlConnection.Close();
+            }
+            return ObjEmployeeList;
         }
 
         public bool Add(Employee ObjNewEmployee)
         {
-            ObjEmployeesList.Add(ObjNewEmployee);
-            return true;
+            bool IsAdded = false;
+            if (ObjNewEmployee.Age < 20 || ObjNewEmployee.Age > 40)
+                throw new ArgumentException("Invalid limit of age");
+            try
+            {
+                ObjSqlCommand.Parameters.Clear();
+                ObjSqlCommand.CommandText = "udp_AddNewEmployee";
+                ObjSqlCommand.Parameters.AddWithValue("@Id", ObjNewEmployee.Id);
+                ObjSqlCommand.Parameters.AddWithValue("@Name", ObjNewEmployee.Name);
+                ObjSqlCommand.Parameters.AddWithValue("@Age", ObjNewEmployee.Age);
+
+                ObjSqlConnection.Open();
+
+                int NoOfRowsAffected = ObjSqlCommand.ExecuteNonQuery();
+                IsAdded = NoOfRowsAffected > 0;
+                
+                
+            }
+            catch(SqlException ex)
+            {
+                throw new ArgumentException(ex.Message);
+            }
+            finally
+            {
+                ObjSqlConnection.Close();
+            }
+            return IsAdded;
         }
 
         public bool Update(Employee ObjEmployeeToUpdate)
         {
             bool IsUpdated = false;
-
-            for(int index = 0; index < ObjEmployeesList.Count; index++)
+            try
             {
-                if(ObjEmployeesList[index].Id == ObjEmployeeToUpdate.Id)
-                {
-                    ObjEmployeesList[index].Name = ObjEmployeeToUpdate.Name;
-                    ObjEmployeesList[index].Age = ObjEmployeeToUpdate.Age;
-                    IsUpdated = true;
-                    break;
-                }
-            }
+                ObjSqlCommand.Parameters.Clear();
+                ObjSqlCommand.CommandText = "udp_UpdateEmployee";
+                ObjSqlCommand.Parameters.AddWithValue("@Id", ObjEmployeeToUpdate.Id);
+                ObjSqlCommand.Parameters.AddWithValue("@Name", ObjEmployeeToUpdate.Name);
+                ObjSqlCommand.Parameters.AddWithValue("@Age", ObjEmployeeToUpdate.Age);
 
+                ObjSqlConnection.Open();
+
+                int NoOfRowsAffected = ObjSqlCommand.ExecuteNonQuery();
+                IsUpdated = NoOfRowsAffected > 0;
+            }
+            catch(SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                ObjSqlConnection.Close();
+            }
             return IsUpdated;
         }
 
         public bool Delete(int id)
         {
             bool IsDeleted = false;
-
-            for(int index=0; index<=ObjEmployeesList.Count; index++)
+            try
             {
-                if (ObjEmployeesList[index].Id == id)
-                {
-                    ObjEmployeesList.Remove(ObjEmployeesList[index]);
-                    IsDeleted = true;
-                    break;
-                }
+                ObjSqlCommand.Parameters.Clear();
+                ObjSqlCommand.CommandText = "udp_DeleteEmployee";
+                ObjSqlCommand.Parameters.AddWithValue("@Id", id);
+
+                ObjSqlConnection.Open();
+
+                int NoOfRowsAffected = ObjSqlCommand.ExecuteNonQuery();
+                IsDeleted = NoOfRowsAffected > 0;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                ObjSqlConnection.Close();
             }
             return IsDeleted;
         }
 
         public Employee Search(int id)
         {
-            return ObjEmployeesList.FirstOrDefault(e => e.Id == id);
+            Employee ObjEmployee = null;
+            try
+            {
+                ObjSqlCommand.Parameters.Clear();
+                ObjSqlCommand.CommandText = "udp_SearchEmployeeById";
+                ObjSqlCommand.Parameters.AddWithValue("@Id", id);
+
+                ObjSqlConnection.Open();
+
+                var ObjSqlDataReader = ObjSqlCommand.ExecuteReader();
+                if (ObjSqlDataReader.HasRows)
+                {
+                    ObjSqlDataReader.Read();
+                    ObjEmployee = new Employee();
+                    ObjEmployee.Id = ObjSqlDataReader.GetInt32(0);
+                    ObjEmployee.Name = ObjSqlDataReader.GetString(1);
+                    ObjEmployee.Age = ObjSqlDataReader.GetInt32(2);
+                }
+                ObjSqlDataReader.Close();
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                ObjSqlConnection.Close();
+            }
+            return ObjEmployee;
         }
     }
 }
--- MVVMEmployee/ViewModels/EmployeeViewModel.cs
+++ MVVMEmployee/ViewModels/EmployeeViewModel.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using MVVMEmployee.Models;
+using MVVMEmployee.Commands;
 namespace MVVMEmployee.ViewModels
 {
     public class EmployeeViewModel : INotifyPropertyChanged
@@ -24,17 +26,21 @@ namespace MVVMEmployee.ViewModels
             ObjEmployeeService = new EmployeeService();
             loadData();
             CurrentEmployee = new Employee();
+            saveCommand = new RelayCommand(Save);
+            searchCommand = new RelayCommand(Search);
+            deleteCommand = new RelayCommand(Delete);
+            updateCommand = new RelayCommand(Update);
         }
         #region Display Operations
-        private List<Employee> employeesList;
-        public List<Employee> EmployeesList
+        private ObservableCollection<Employee> employeesList;
+        public ObservableCollection<Employee> EmployeesList
         {
             get { return employeesList; }
             set { employeesList = value; OnPropertyChanged("EmployeesList"); }
         }
         public void loadData()
         {
-            EmployeesList = ObjEmployeeService.GetAll();
+            EmployeesList = new ObservableCollection<Employee>(ObjEmployeeService.GetAll());
         }
         #endregion
         private Employee currentEmployee;
@@ -43,5 +49,131 @@ namespace MVVMEmployee.ViewModels
             get { return currentEmployee; }
             set { currentEmployee = value; OnPropertyChanged("CurrentEmployee"); }
         }
+
+        private string message;
+
+        public string Message
+        {
+            get { return message; }
+            set { message = value; OnPropertyChanged("Message"); }
+        }
+
+
+        #region Save operations
+        private RelayCommand saveCommand;
+
+        public RelayCommand SaveCommand
+        {
+            get { return saveCommand; }
+        }
+
+        public void Save()
+        {
+            try
+            {
+                var IsSaved = ObjEmployeeService.Add(CurrentEmployee);
+                loadData();
+                if (IsSaved)
+                    Message = "Employee save successfully!";
+                else
+                    Message = "Failed to save Employee!";
+            }
+            catch(Exception ex)
+            {
+                Message = ex.Message;
+            }
+        }
+        #endregion
+
+        #region Search Operations
+
+        private RelayCommand searchCommand;
+
+        public RelayCommand SearchCommand
+        {
+            get { return searchCommand; }
+        }
+
+        public void Search()
+        {
+            try
+            {
+                var ObjEmployee = ObjEmployeeService.Search(CurrentEmployee.Id);
+                if(ObjEmployee != null)
+                {
+                    CurrentEmployee.Name = ObjEmployee.Name;
+                    CurrentEmployee.Age = ObjEmployee.Age;
+                    Message = "Employee Found!";
+                }
+                else
+                {
+                    Message = "Employee Not Found!";
+                }
+            }
+            catch(Exception ex)
+            {
+                Message = ex.Message;
+            }
+        }
+        #endregion
+
+        #region Delete Operations
+
+        private RelayCommand deleteCommand;
+
+        public RelayCommand DeleteCommand
+        {
+            get { return deleteCommand; }
+        }
+
+        public void Delete()
+        {
+            try
+            {
+                var IsDeleted = ObjEmployeeService.Delete(CurrentEmployee.Id);
+                if (IsDeleted)
+                    Message = "Employee Deleted!";
+                else
+                    Message = "Failed to Delete1";
+            }
+            catch(Exception ex)
+            {
+                Message = ex.Message;
+            }
+        }
+
+        #endregion
+
+        #region Update Operations
+
+        private RelayCommand updateCommand;
+
+        public RelayCommand UpdateCommand
+        {
+            get { return updateCommand; }
+        }
+
+        public void Update()
+        {
+            try
+            {
+                var IsUpdated = ObjEmployeeService.Update(CurrentEmployee);
+                if (IsUpdated)
+                {
+                    Message = "Employee is updated!";
+                    loadData();
+                }
+                else
+                {
+                    Message = "Update Failed!";
+                }
+            }
+            catch(Exception ex)
+            {
+                Message = ex.Message;
+            }
+        }
+
+        #endregion
     }
 }
